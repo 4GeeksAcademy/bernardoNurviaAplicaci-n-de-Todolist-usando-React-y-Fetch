3e2a5f54be7a36a8ec469c8e85d1fa6feb6158ab
Bernardo Patiño <bap_link@hotmail.com>
2024-07-24 16:37:59 +0000

    add check in task

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,18 +10,18 @@ const Home = () => {
 
   const agregarTarea = (evento) => {
     evento.preventDefault(); // Previene el comportamiento por defecto del formulario
-    todo ==="" ?  
-	alert("Please enter task") : 
-	setTodos([...todos, todo]); // Agrega la tarea actual al array de tareas
+    todo === "" ? alert("Please enter task") : setTodos([...todos, todo]); // Agrega la tarea actual al array de tareas
     setTodo(""); // Limpia el input
   };
   const quitarTarea = (index) => {
     const nuevasTareas = [...todos];
     nuevasTareas.splice(index, 1);
     setTodos(nuevasTareas);
-  }
-    return (
-      <div className="text-center container">
+  };
+
+  return (
+    <div>
+      <div className="text-center container card mt-3 ">
         <h1 className="text-center mt-5">ToDo List</h1>
         <form onSubmit={agregarTarea}>
           <div className="mb-3">
@@ -39,35 +39,53 @@ const Home = () => {
               value={todo}
             />
             <button className="btn btn-success mt-3 mb-3" type="submit">
-			Add task
+              Add task
             </button>
             <p>ToDo´s:{todo}</p>
-			
-            <div className="container text-center col">
-              <ul className="list-group">
-                {todos.map((tarea, index) => (
-                  <li key={index} className="list-group-item">
-                    {tarea}
-                    <button
-                      type="button"
-                      class="btn-close m-auto"
-                      onClick={() => {
-                        quitarTarea();
-                      }}
-                      aria-label="Close"
-                    ></button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div id="emailHelp" className="form-text">
-              Hecho por Bernardo Nurvia
-            </div>
           </div>
         </form>
       </div>
-	);
-  
+      <div className="container text-center mt-2 col">
+        <ul className="list-group">
+          {todos.map((tarea, index) => (
+            //   <li key={index} className="bg-info list-group-item card ">
+            //     {tarea}
+            //     <button
+            //       type="button"
+            //       class="btn-close m-auto"
+            //       onClick={() => {
+            //         quitarTarea();
+            //       }}
+            //       aria-label="Close"
+            //     ></button>
+            //   </li>
+            <div class="form-check bg-info list-group-item ">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label class="form-check-label" for="flexCheckDefault">
+                {tarea}
+              </label>
+              <button
+                type="button"
+                class="btn-close m-auto"
+                onClick={() => {
+                  quitarTarea();
+                }}
+                aria-label="Close"
+              ></button>
+            </div>
+          ))}
+        </ul>
+      </div>
+      <div id="emailHelp" className="fixed-bottom text-center">
+        Made by Bernardo Nurvia
+      </div>
+    </div>
+  );
 };
 
 export default Home;
